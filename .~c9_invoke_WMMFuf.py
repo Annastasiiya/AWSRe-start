637e0c73b4a95efc7a1f4ff4411a11fe967636a3
Annastasiiya <38215401+Annastasiiya@users.noreply.github.com>
2023-01-17 10:41:24 +0200

    Add files via upload

---
+++ .~c9_invoke_WMMFuf.py
@@ -0,0 +1,12 @@
+WorkHours_1 = 7,17
+WorkHours_2 = 0,7
+WorkHours_3 = 17,23
+UserAsk = input("Check time: ")
+if UserAsk == WorkHours_1:
+    print("in")
+elif UserAsk == WorkHours_2:
+    print("out")
+elif UserAsk == WorkHours_3:
+    print("out")
+else:
+    print("Check")
